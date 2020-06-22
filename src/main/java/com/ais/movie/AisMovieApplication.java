fd88d9342f3a303f4aa781dec8edff991c1000c1
Matheus Henrique Braga Caldeira <matheus.caldeira@maisvida.med.br>
2020-06-22 17:54:27 -0300

    commit inicial

---
+++ src/main/java/com/ais/movie/AisMovieApplication.java
@@ -0,0 +1,13 @@
+package com.ais.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ais/movie/AisMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ais.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
